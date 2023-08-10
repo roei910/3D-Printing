88d396f4e9b8feae13c09b297c056b5db50e9501
roei910 <74923231+roei910@users.noreply.github.com>
2023-08-10 18:31:23 +0300

    added cors

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log("Starting server");
+const express = require('express');
+const cors = require('cors');
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.listen(port, (req, res) => {
+    console.log(`app listening on port ${port}`);
+})
