a6a5bf9ba8a4aa05a0a443308e0822318b7a4444
roei910 <74923231+roei910@users.noreply.github.com>
2023-08-11 12:59:48 +0300

    learning express

--- server - node js/index.js
+++ server - node js/index.js
@@ -3,20 +3,33 @@ const cors = require('cors');
 const port = 3000;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(express.static('public')); //using static view engine, using html files
+app.use(express.urlencoded({ extended: true}));
 
-app.get('/', (req, res) => {
-    // res.send('welcome');
-    // res.status(200).send('Welcome');
-    // res.status(200).json({ 'success': true });
-    // res.json({ 'success': true }); //sends a status 200 as default
-    // res.download('index.js');
-    res.render('index', { name: 'roei'});
-});
+app.set('view engine', 'ejs'); //using dynamic view engine, using ejs from views folder
+app.use(logger);
+
+// app.get('/', (req, res) => {
+//     // res.send('welcome');
+//     // res.status(200).send('Welcome');
+//     // res.status(200).json({ 'success': true });
+//     // res.json({ 'success': true }); //sends a status 200 as default
+//     // res.download('index.js');
+//     res.render('index', { name: 'roei'});
+// }); //for using dynamic views with ejs
+
+// app.get('/', logger, logger, logger ,(req, res) => {
+//     res.render('index', { name: 'roei'});
+// }); //running a certain request with specific logger, you can run multiple loggers like showed above
 
 const userRouter = require('./routes/users');
 
 app.use('/users', userRouter);
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 app.listen(port, (req, res) => {
     console.log(`app listening on port ${port}`);
--- server - node js/routes/users.js
+++ server - node js/routes/users.js
@@ -1,12 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Users List");
-})
+router.get("/", (req, res) => {
+  console.log(req.query.name);
+  res.send("Users List");
+});
 
-router.get('/new', (req, res) => {
-    res.send("New User");
-})
+router.get("/new", (req, res) => {
+  //   res.send("New User");
+  res.render("users/new", { firstName: "Test" });
+});
 
+router.post("/", (req, res) => {
+  const valid = true;
+  if (valid) {
+    users.push({ name: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`user get with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`user update with id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`user delete with id ${req.params.id}`);
+  });
+
+const users = [{ name: "Kyle" }, { name: "Sally" }];
+
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 module.exports = router;
